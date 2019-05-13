fad8fc15f4515d9cef8f372a0f07fbf94fbf40ac
Aaron Chantrill <aaron.chantrill@dottywood.org>
2019-05-13 17:02:04 -0400

    Working on navigation

--- JasperWeb.py
+++ JasperWeb.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python2
+
+# This version of JasperWeb allows the user to step through one sample at a time, instead of returning a table full of samples.
+# My assumption is that for the most part, you only need to validate the record once, after that it should automatically skip
+# all the records that have already been verified.
+
 # -*- coding: utf-8 -*-
 from datetime import datetime
 import json
@@ -7,10 +12,9 @@ from SocketServer import ThreadingMixIn
 import re
 import sqlite3
 import urlparse
+from urllib2 import unquote
 import os,sys
-applicationdir = os.path.join(os.path.dirname(os.path.abspath(__file__)),"jasper")
-sys.path.insert(0,applicationdir)
-import paths
+from jasper import paths
 
 def application(environ,start_response):
     print( "PATH_INFO=%s"%environ["PATH_INFO"] )
@@ -24,27 +28,46 @@ def application(environ,start_response):
         wavfile=""
         action=""
         rowID=""
+        first_rowID=""
+        prev_rowID=""
+        next_rowID=""
         transcription=""
+        result=""
+        post_data=""
+        reQS=re.compile("([^=]+)=([^&]*)&?")
         
         # gather parameters from GET
         if( environ["QUERY_STRING"] ):
-            reQS=re.compile("([^=]+)=([^&]*)&?")
             for namevalue in reQS.findall(environ["QUERY_STRING"]):
-                if( namevalue[0]=="wavfile" ):
+                if( namevalue[0].lower()=="wavfile" ):
                     wavfile=os.path.join(audiolog_path,namevalue[1])
+                if( namevalue[0].lower()=="rowid" ):
+                    rowID=namevalue[1]
+
         # gather parameters from POST
         content_length=0
         if( environ['CONTENT_LENGTH'] ):
             content_length = int(environ['CONTENT_LENGTH'])
+            post_data=environ['wsgi.input'].read(content_length)
+            # Parse it out
+            for namevalue in reQS.findall(post_data):
+                if( namevalue[0].lower()=="result" ):
+                    result=namevalue[1].lower()
+                if( namevalue[0].lower()=="transcription" ):
+                    transcription=unquote(namevalue[1].replace('+',' '))
+        """
         if( content_length ):
             request = json.loads(environ['wsgi.input'].read(content_length))
             for key in request:
                 if( key.lower()=="action" ):
                     action=request[key]
                 if( key.lower()=="rowid" ):
-                    rowID=int(request[key])
+                    rowID=request[key]
                 if( key.lower()=="transcription" ):
                     transcription=request[key]
+                if( key.lower()=="result" ):
+                    result=request[key]
+        """
         # Handle the request
         # serve a .wav file
         if( len(wavfile) and os.path.isfile(wavfile) ):
@@ -54,29 +77,39 @@ def application(environ,start_response):
             w.close()
             return ret
         else:
-            # delete a record from the database
-            if( action.lower()=="delete" ):
-                start_response('200 OK',[('content-type','application/json;charset=utf-8')])
-                conn=sqlite3.connect(audiolog_db)
-                c=conn.cursor()
-                c.execute("delete from audiolog where RowID=%d"%rowID)
-                conn.commit()
-                conn.close()
-                ret=json.dumps({"message":"SUCCESS;Deleted %s"%str(rowID)})
-                return ret
-            # update a transcription
-            if( action.lower()=="update" ):
-                start_response('200 OK',[('content-type','application/json;charset=utf-8')])
-                conn=sqlite3.connect(audiolog_db)
-                c=conn.cursor()
-                c.execute( "update audiolog set verified_transcription='%s',reviewed=strftime('%%Y-%%m-%%d %%H:%%M:%%S','now') where RowID=%d"%(transcription,rowID) )
-                conn.commit()
-                conn.close()
-                ret=json.dumps({"message":"SUCCESS;Updated %s"%str(rowID)})
-                return ret
-            else:
-                # Return the main table
+            conn=sqlite3.connect(audiolog_db)
+            c=conn.cursor()
+            # Return the main page
+            c.execute("select RowID from audiolog where reviewed='' order by RowID asc limit 1")
+            row=c.fetchone()
+            if( row is not None ):
+                first_rowID=str(row[0])
+            if( rowID=="" ):
+                rowID=first_rowID
+            print( "rowID=%s"%rowID )
+            # get the previous rowid and next rowid
+            c.execute("select RowID from audiolog where RowID<:RowID order by RowID desc limit 1",{"RowID":rowID})
+            row=c.fetchone()
+            if( row is not None ):
+                prev_rowID=str(row[0])
+            c.execute("select RowID from audiolog where RowID>:RowID order by RowID asc limit 1",{"RowID":rowID})
+            row=c.fetchone()
+            if( row is not None ):
+                next_rowID=str(row[0])
+            if( result.lower()=="correct" ):
+                c.execute("update audiolog set verified_transcription=transcription,reviewed=:reviewed where RowID=:RowID",{"reviewed":datetime.now().strftime("%Y-%m-%d_%H-%M-%S"),"RowID":rowID} )
+            if( result.lower()=="update" ):
+                if( len(transcription) ):
+                    c.execute("update audiolog set verified_transcription=:verified_transcription,reviewed=:reviewed where RowID=:RowID",{"verified_transcription":transcription,"reviewed":datetime.now().strftime("%Y-%m-%d_%H-%M-%S"),"RowID":rowID} )
+                else:
+                    c.execute("update audiolog set type='noise',verified_transcription='',reviewed=:reviewed where RowID=:RowID",{"reviewed":datetime.now().strftime("%Y-%m-%d_%H-%M-%S"),"RowID":rowID} )
+            if( result.lower()=="nothing" ):
+                c.execute("update audiolog set type='noise',verified_transcription='',reviewed=:reviewed where RowID=:RowID",{"reviewed":datetime.now().strftime("%Y-%m-%d_%H-%M-%S"),"RowID":rowID} )
+            conn.commit()
+            conn.close()
+            if( len(first_rowID) ):
                 ret=[]
+                # Get the first unvalidated row from the database
                 start_response('200 OK',[('content-type','text/html;charset=utf-8')])
                 ret.append('<html><head><title>Test</title>')
                 ret.append("""
@@ -84,6 +117,7 @@ def application(environ,start_response):
     // Submit an updated transcription to the server. Upon success, make the "revert" button inactive
     function UpdateTranscription(RowID){
         var Transcription=document.getElementById("transcription_"+RowID).value;
+        alert( "Transcription="+Transcription );
         var xhttp=new XMLHttpRequest();
         xhttp.onreadystatechange=function(){
             if( this.readyState==4 && this.status==200 ){
@@ -92,7 +126,6 @@ def application(environ,start_response):
                 if( message=="SUCCESS;Updated "+RowID ){
                     // disable reset button
                     document.getElementById("reset_"+RowID).disabled=true;
-                    document.getElementById("r"+RowID).parentNode.removeChild(document.getElementById("r"+RowID));
                 }else{
                     //alert( "message="+message );
                 }
@@ -128,37 +161,44 @@ def application(environ,start_response):
                 
                 conn=sqlite3.connect(audiolog_db)
                 c=conn.cursor()
-                c.execute("select rowid,datetime,filename,type,transcription,verified_transcription,speaker,reviewed from audiolog where reviewed=''")
-                rows=c.fetchall()
-                ret.append("""<table id="audioclips" border=1 width="100%">\n""")
-                for row in rows:
-                    print( "%s"%str(row) )
-                    RowID=int(row[0])
-                    # Convert this to a datetime expression
-                    #DateTime=datetime.strptime(row[1],'%Y-%m-%d %H:%M:%S')
-                    Filename=str(row[2])
-                    Type=str(row[3])
-                    Transcription=str(row[4])
-                    if( len(row[7]) ):
-                        Transcription=str(row[5])
-                    Speaker=str(row[6])
-                    #Reviewed=datetime.strptime(row[7],'%Y-%m-%d %H:%M:%S')
-                    ret.append( """<tr id="r%d">"""%RowID )
-                    ret.append( """<td>%s</td>"""%Type )
-                    ret.append( """<td style="width:320px"><audio controls src="?wavfile=%s" type="audio/wav"/></td>"""%Filename )
-                    ret.append( """<form method="get"><td><input id="transcription_%d" type="text" name="transcription" value="%s" style="width:90%%;margin:20px" onchange="document.getElementById('reset_%d').disabled=false" /></td>"""%(RowID,Transcription,RowID) )
-                    ret.append( """<td><input type="button" name="action" value="Verify" onclick="UpdateTranscription(%d)"/><input id="reset_%d" type="reset" disabled=true onclick="this.disabled=true"/></td></form>"""%(RowID,RowID) )
-                    ret.append( """<form><td><input id="delete_%d" type="button" name="action" value="Delete" onclick="DeleteAudio(%d);"/></td></form>"""%(RowID,RowID) )
-                    ret.append( """</tr>\n""" )
-                ret.append("</table>\n")
+                c.execute("select datetime,filename,type,transcription,verified_transcription,speaker,reviewed from audiolog where rowid=:RowID",{"RowID":rowID})
+                row=c.fetchone()
                 conn.close()
+                Filename=str(row[1])
+                Type=str(row[2])
+                Original_Transcription=str(row[3])
+                Transcription=str(row[3])
+                if( len(row[6]) ):
+                    Transcription=str(row[4])
+                Speaker=str(row[5])
+                ret.append("""<ul>""")
+                ret.append("""<li>post_data: %s</li>"""%post_data)
+                ret.append("""<li>Result: %s</li>"""%result)
+                if( result=="update" ):
+                    ret.append("""<li>transcription: %s</li>"""%transcription)
+                ret.append("""</ul>""")
+                ret.append("""<h1>Jasper</h1>""")
+                ret.append("""<audio controls type="audio/wav"><source src="?wavfile=%s" /></audio><br />"""%Filename )
+                ret.append("""Jasper heard "<span style="font-weight:bold">%s</span>"<br />"""%Original_Transcription )
+                ret.append("""What did you hear?<br />""")
+                ret.append("""<form method="POST">""")
+                ret.append("""<input type="radio" name="result" value="correct" onclick="document.getElementById('update_transcription').disabled=true"/> The transcription is correct. I heard the same thing<br />""")
+                ret.append("""<input type="radio" name="result" value="update" onclick="document.getElementById('update_transcription').disabled=false"/> The transcription is not correct. This is what I heard: <input id="update_transcription" type="text" name="transcription" value="%s" disabled=true/><br />"""%Transcription)
+                ret.append("""<input type="radio" name="result" value="nothing" onclick="document.getElementById('update_transcription').disabled=true"/> This was just noise with no voices.<br />""")
+                ret.append("""<input type="submit" value="Submit"/><br />""")
+                if( prev_rowID ):
+                    ret.append("""<input type="button" value="Prev" onclick="goprev()">""")
+                if( next_rowID ):
+                    ret.append("""<input type="button" value="Next" onclick="gonext()">""")
                 ret.append( """</body></html>""" )
                 return ret
+            else:
+                return ["<html><head><title>Nothing to validate</title></head><body><h1>Nothing to validate</h1></body></html>"]
 
 class ThreadingWSGIServer(ThreadingMixIn,wsgiref.simple_server.WSGIServer):
     pass
 
-port=8000
+port=8080
 print("Listening on port %d"%port)
-server=wsgiref.simple_server.make_server('',8000,application,ThreadingWSGIServer)
+server=wsgiref.simple_server.make_server('',port,application,ThreadingWSGIServer)
 server.serve_forever()
